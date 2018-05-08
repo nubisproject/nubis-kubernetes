e64222a647444b9ea75fac19636064733a4ce313
limed <limed@mozilla.com>
2018-05-08 13:54:28 -0700

    Fixing typos

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -102,19 +102,19 @@ module "kops_cluster" {
   master-image                 = "${var.ami}"
   master-additional-sgs        = "${local.security_groups}"
   master-additional-sgs-count  = "${local.security_groups_count}"
-  master-addidtional-user-data = "${data.template_file.user_data_cloudconfig.rendered}"
+  master-additional-user-data  = "${data.template_file.user_data_cloudconfig.rendered}"
 
   # Bastion
   bastion-image                 = "${var.ami}"
   bastion-additional-sgs        = "${local.security_groups}"
   bastion-additional-sgs-count  = "${local.security_groups_count}"
-  bastion-addidtional-user-data = "${data.template_file.user_data_cloudconfig.rendered}"
+  bastion-additional-user-data  = "${data.template_file.user_data_cloudconfig.rendered}"
 
   # First minion instance group
   minion-image                 = "${var.ami}"
   minion-additional-sgs        = "${local.security_groups}"
   minion-additional-sgs-count  = "${local.security_groups_count}"
-  minion-addidtional-user-data = "${data.template_file.user_data_cloudconfig.rendered}"
+  minion-additional-user-data  = "${data.template_file.user_data_cloudconfig.rendered}"
   min-minions                  = 2
 }
 
