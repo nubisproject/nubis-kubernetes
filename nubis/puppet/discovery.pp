792d716b4c98278ef054dec55246b77b11e547da
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-05-14 14:00:19 -0400

    Cleanup service-discovery service name

--- nubis/puppet/discovery.pp
+++ nubis/puppet/discovery.pp
@@ -1,6 +1,7 @@
 include nubis_discovery
 
-nubis::discovery::service { $project_name:
+# Strip the nubis- portion of project name, just to be tidy
+nubis::discovery::service { split($project_name, '-')[1]:
   tags => [
     '%%PURPOSE%%',
     'kubelet',
