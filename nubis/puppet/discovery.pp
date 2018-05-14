523142da0219f8a7a09d342b456c80850a5fb01a
limed <limed@mozilla.com>
2018-05-14 16:07:07 -0700

    Service discovery for kube metrics componenents

--- nubis/puppet/discovery.pp
+++ nubis/puppet/discovery.pp
@@ -9,3 +9,22 @@ nubis::discovery::service { split($project_name, '-')[1]:
   http => 'http://localhost:10255/healthz',
 }
 
+# Kubelet metric discovery
+nubis::discovery::service { 'kubelet-metrics':
+  tags => [
+    '%%PURPOSE%%',
+    'metrics',
+  ],
+  http => 'http://localhost:10255/metrics',
+}
+
+# This exists over here because of how we build
+# AMIs we build 1 AMI for master and nodes therefore
+# its hard to differentiate between master and node during packer builds
+file { '/etc/nubis.d/02-kube-sd':
+  ensure => file,
+  owner  => root,
+  group  => root,
+  mode   => '0755',
+  source => 'puppet:///nubis/files/kube-sd.sh',
+}
