f9e8b320208c697ca664eb905589d6c7e22fbcb8
limed <limed@mozilla.com>
2018-05-31 09:42:26 -0700

    Initial attempt to make nubis-kubernetes able to deploy via the account deploy process

--- nubis/terraform/variables.tf
+++ nubis/terraform/variables.tf
@@ -1,3 +1,5 @@
+variable "enabled" {}
+
 variable "account" {}
 
 variable "region" {}
@@ -40,4 +42,16 @@ variable "consul_acl_token" {
 variable "ssh_pubkey" {
   type    = "string"
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0/tR0k8b6gIQpd6IHyEJdzmGur60ShmOdQGpBoF7IPMBWTHgc5w3CTcqvK6aJ6GpZHyybi9D9EON4+1WZTf9tcsdUP8kyVOs66sw26FWeCri2k1zomsGP9Ysr3bSUe3dpi5vipk1PDXpaD6wYs/eEtQxO1U1wRCGEGclRdh5G8UbOMwrPIHvQd77ma5RyXzd36htzFtsKnuyTtG7xHGPphzVqLZmiDZeyxbr3mCuaMBW30syEKviiVbMo4RsmDqzR3N2ltInGKYgZpCW7fd7KrZL/G0oi/XS+Up5MvmYSsP2tYNx909CWFpWDsXEPMNddl7ZYizHXLbLexU8+0h5j nubis"
+}
+
+variable "kubernetes_master_type" {
+  default = "c4.large"
+}
+
+variable "kubernetes_node_type" {
+  default = "t2.medium"
+}
+
+variable "kubernetes_node_minimum" {
+  default = "2"
 }
