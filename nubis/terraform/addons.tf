d111f2f3cb51919905e7b279443324dfd73acc1e
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-06-06 10:11:58 -0400

    Deploy kube2iam
    Fixes #55

--- nubis/terraform/addons.tf
+++ nubis/terraform/addons.tf
@@ -1,7 +1,8 @@
 # These need to not be too long
 locals {
   dashboard_etag = "${substr(md5(file("${path.module}/files/addons/dashboard-sso/manifest.yaml")), 0, 6)}"
-  addons_etag    = "${local.dashboard_etag}"
+  kube2iam_etag  = "${substr(md5(file("${path.module}/files/addons/kube2iam/manifest.yaml.tmpl")), 0, 6)}"
+  addons_etag    = "${local.dashboard_etag}-${local.kube2iam_etag}"
 }
 
 resource "aws_s3_bucket_object" "nubis_addon" {
@@ -17,5 +18,24 @@ resource "aws_s3_bucket_object" "dashboard_manifest" {
   key          = "kubernetes.${module.info.hosted_zone_name}/addons/nubis/${local.addons_etag}/dashboard-sso/manifest.yaml"
   source       = "${path.module}/files/addons/dashboard-sso/manifest.yaml"
   etag         = "${local.dashboard_etag}"
+  content_type = "text/yaml"
+}
+
+data "aws_caller_identity" "current" {}
+
+data "template_file" "kube2iam_manifest" {
+  template = "${file("${path.module}/files/addons/kube2iam/manifest.yaml.tmpl")}"
+
+  vars = {
+    DEFAULT_ROLE = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/nubis/readonly"
+    VERSION      = "0.10.0"
+  }
+}
+
+resource "aws_s3_bucket_object" "kube2iam_manifest" {
+  bucket       = "${module.kops_bucket.name}"
+  key          = "kubernetes.${module.info.hosted_zone_name}/addons/nubis/${local.addons_etag}/kube2iam/manifest.yaml"
+  content      = "${data.template_file.kube2iam_manifest.rendered}"
+  etag         = "${local.kube2iam_etag}"
   content_type = "text/yaml"
 }
