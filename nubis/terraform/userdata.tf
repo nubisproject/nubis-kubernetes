21fc3ae8d22f54388b939e1e2adb9750764d0836
Nubisproject (Mozilla) <nubis-users@mozilla.com>
2018-07-20 16:01:01 +0000

    Update pinned Terraform version for v2.3.0 release [skip ci]

--- nubis/terraform/userdata.tf
+++ nubis/terraform/userdata.tf
@@ -1,5 +1,5 @@
 module "userdata_master" {
-  source            = "github.com/nubisproject/nubis-terraform//worker/userdata?ref=develop"
+  source            = "github.com/nubisproject/nubis-terraform//worker/userdata?ref=v2.3.0"
   region            = "${var.region}"
   nubis_domain      = "${var.nubis_domain}"
   arena             = "${var.arena}"
@@ -23,7 +23,7 @@ data "template_file" "userdata_master" {
 }
 
 module "userdata_node" {
-  source            = "github.com/nubisproject/nubis-terraform//worker/userdata?ref=develop"
+  source            = "github.com/nubisproject/nubis-terraform//worker/userdata?ref=v2.3.0"
   region            = "${var.region}"
   nubis_domain      = "${var.nubis_domain}"
   arena             = "${var.arena}"
@@ -47,7 +47,7 @@ data "template_file" "userdata_node" {
 }
 
 module "userdata_bastion" {
-  source            = "github.com/nubisproject/nubis-terraform//worker/userdata?ref=develop"
+  source            = "github.com/nubisproject/nubis-terraform//worker/userdata?ref=v2.3.0"
   region            = "${var.region}"
   nubis_domain      = "${var.nubis_domain}"
   arena             = "${var.arena}"
